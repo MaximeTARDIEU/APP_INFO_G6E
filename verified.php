f9cc2a70aafc48aa4495083f396b25cb3337635e
sman_steel <59889389+smansteel@users.noreply.github.com>
2023-01-03 09:40:32 +0100

    initial commit

---
+++ verified.php
@@ -0,0 +1,28 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+<link rel="stylesheet" href="login.css">
+
+<body>
+
+    <div class="fb-page">
+            <?php include("header.php"); ?>
+
+        <div class="box">
+
+            <div class="bg-image">
+                <img src="sources/captair.png" class="logo">
+                <div class="text">
+                    La vérification d'adresse mail a fonctionné.<br> Nous vous invitons à rejoindre la page mon compte pour accéder a vos données.
+                </div>
+                <br>
+                <a class="newacc" href="/login.php" class="newacc">Se Connecter</a><br>
+            </div>
+
+                
+        </div>
+
+        <?php include("footer.php"); ?>
+    </div>
+</body>
+
+</html>
